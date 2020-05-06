1d637ef25b10c33c293b972be885f4838ae4ca82
ing <inyoman_gurnitha@yahoo.com>
2020-05-06 17:03:28 +0700

    02.5. Adding Meals List View

--- app_meals/models.py
+++ app_meals/models.py
@@ -1,4 +1,4 @@
-# meals/models.py
+# app_meals/models.py
 from django.db import models
 from django.utils.text import slugify
 
--- app_meals/views.py
+++ app_meals/views.py
@@ -1,3 +1,12 @@
+# app_meals/views.py
 from django.shortcuts import render
+from .models import Meals
 
-# Create your views here.
+def meal_list(request):
+    meal_list = Meals.objects.all()
+    context = {'meal_list': meal_list}
+
+    return render(request, 'app_meals/meal_list.html', context)
+
+def meal_detail(request, slug):
+    pass
