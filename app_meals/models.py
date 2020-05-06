6d620a25d2442ad6ddae414b686309eb7e9ab998
ing <inyoman_gurnitha@yahoo.com>
2020-05-06 16:16:13 +0700

    02.4. Adding Slug in Meals

--- app_meals/models.py
+++ app_meals/models.py
@@ -1,6 +1,6 @@
 # meals/models.py
-
 from django.db import models
+from django.utils.text import slugify
 
 class Meals(models.Model):
     name = models.CharField(max_length=50)
@@ -9,6 +9,23 @@ class Meals(models.Model):
     price = models.DecimalField(max_digits=5 , decimal_places=2)
     preperation_time =models.IntegerField()
     image = models.ImageField(upload_to='meals/')
+    slug = models.SlugField(blank=True, null=True)
 
+    # add name automaticall to slug if the slug field is empty
+    def save(self , *args , **kwargs):
+        # if the slug field is empty, but name field is NOT empty
+        if not self.slug and self.name :
+            # then add the name to slug field
+            self.slug = slugify(self.name)
+            # then save the name to slug
+        super(Meals , self).save(*args , **kwargs)
+
+    # Convert table's name from Mealss to Meals
+    # Note: by default django will add 's' to Model name ('Meals' to 'Mealss' ) automatically
+    class Meta:
+        verbose_name = 'meal'
+        verbose_name_plural = 'meals'
+
+    # Convert object of a name to a string
     def __str__(self):
         return self.name
