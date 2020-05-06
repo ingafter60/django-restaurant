8897b6a02beb61d80255ee4be6c62e431108cb9e
ing <inyoman_gurnitha@yahoo.com>
2020-05-06 20:10:42 +0700

    02.6. Adding Meals Detail View

--- app_meals/views.py
+++ app_meals/views.py
@@ -4,9 +4,14 @@ from .models import Meals
 
 def meal_list(request):
     meal_list = Meals.objects.all()
+
     context = {'meal_list': meal_list}
 
     return render(request, 'app_meals/meal_list.html', context)
 
-def meal_detail(request, slug):
-    pass
+def meal_detail(request, slug): # this slug is from the url
+    meal_detail = Meals.objects.get(slug=slug) # these slug are from the model and from ther url
+
+    context = {'meal_detail': meal_detail}
+
+    return render(request, 'app_meals/meal_detail.html', context)
